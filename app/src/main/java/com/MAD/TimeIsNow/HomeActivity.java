4d2b2b5cf46a86f5beabe14567d61a05e2256403
greendog21 <69486663+greendog21@users.noreply.github.com>
2021-01-03 16:45:34 -0600

    front end complete for dark mode

--- app/src/main/java/com/MAD/TimeIsNow/HomeActivity.java
+++ app/src/main/java/com/MAD/TimeIsNow/HomeActivity.java
@@ -10,6 +10,8 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        setSupportActionBar(findViewById(R.id.homeFragment));
+
 
     }
 }
